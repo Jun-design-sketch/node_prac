73119a559d569e8a0cb3b7a50ff1fd12b60ccfc2
jeong <jeong.junhueck@gmail.com>
2025-07-14 09:13:17 +0900

    file transfer batch

---
+++ node-project/bin/shell/transfer-file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "[INFO] transfer file start..."
+# スクリプトの実行ディレクトリに移動し、現在のパスを取得
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# 現在のパス
+cd "$SCRIPT_DIR/.."
+# rootへ
+# TODO: rootなどの情報をまとめる基盤.shが欲しい
+cd "../"
+# エントリポイントへ渡す
+# node bin/index.js csv-grouping data/input.csv data/output.csv
+# TODO asyncコマンドでshレベルで処理できる？
+
+if [ $? -ne 0 ]; then
+  echo "[ERROR] transfer file failed"
+  exit 1
+fi
+
+echo "[INFO] transfer file finished"
