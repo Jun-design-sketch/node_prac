49259f63bffdd2b02dc173917bc254790a531bcc
jeong <jeong.junhueck@gmail.com>
2025-07-19 21:29:54 +0900

    .gitignoreを整理し、config取り込みを追加

--- node-project/bin/shell/base/config.sh
+++ node-project/bin/shell/base/config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 export API_KEY="DUMMY-DUMM1-SKS22"
+export TARGET_TYPE="jpg"
+export DESTINATION_TYPE="webp"
--- node-project/bin/shell/transfer-file.sh
+++ node-project/bin/shell/transfer-file.sh
@@ -2,6 +2,9 @@
 echo "[INFO] transfer file start..."
 # スクリプトの実行ディレクトリに移動し、現在のパスを取得
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# 設定を読み込む
+source $SCRIPT_DIR/base/config.sh
+
 # 現在のパス
 cd "$SCRIPT_DIR/.."
 # rootへ
